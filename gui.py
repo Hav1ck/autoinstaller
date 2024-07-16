25fdd5a58da632c6e5cd6813708f930440868f9b
havic <allk3ra@gmail.com>
2024-07-16 09:05:24 +0200

    added new download options

--- gui.py
+++ gui.py
@@ -13,7 +13,6 @@ class downloads:
         filename = URL.split("/")[-1]
         filename = re.sub(r'[^\w\-_\. ]', '_', filename)
         
-        # Create the downloads directory if it doesn't exist
         if not os.path.exists('downloads'):
             os.makedirs('downloads')
         
@@ -63,12 +62,12 @@ class downloads:
     def start_7zip():
         print("Downloading 7-Zip...")
         file_path = downloads.downloading("https://www.7-zip.org/a/7z2407-x64.exe")
-        downloads.open_file(file_path, admin=True)
+        downloads.open_file(file_path)
 
     @staticmethod
     def start_filterkeysetter():
         print("Downloading FilterKeySetter...")
-        zip_path = downloads.downloading("https://wintools.b-cdn.net/FilterKeysSetter_1.0.zip")
+        zip_path = downloads.downloading("https://wintools.b-cdn.net/FilterKeysSett er_1.0.zip")
         
         with ZipFile(zip_path, 'r') as zip_ref:
             zip_ref.extractall("downloads/filterkeysetter")
@@ -80,6 +79,66 @@ class downloads:
     def start_rainmeter():
         print("Downloading Rainmeter...")
         file_path = downloads.downloading("https://github.com/rainmeter/rainmeter/releases/download/v4.5.18.3727/Rainmeter-4.5.18.exe")
+        downloads.open_file(file_path)
+
+    @staticmethod
+    def start_chrome():
+        print("Downloading Google Chrome...")
+        file_path = downloads.downloading("https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D&iid%3D%7B45542E4C-DFAE-51A1-38F1-1F83E38E3C27%7D&lang%3Den&browser%3D4&usagestats%3D0&appname%3DChrome&needsadmin%3Dprefers/dl/chrome/install/googlechromestandaloneenterprise64.msi")
+        downloads.open_file(file_path)
+
+    @staticmethod
+    def start_notepadpp():
+        print("Downloading Notepad++...")
+        file_path = downloads.downloading("https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.4.8/npp.8.4.8.Installer.x64.exe")
+        downloads.open_file(file_path)
+
+    @staticmethod
+    def start_gimp():
+        print("Downloading GIMP...")
+        file_path = downloads.downloading("https://download.gimp.org/mirror/pub/gimp/v2.10/windows/gimp-2.10.34-setup-3.exe")
+        downloads.open_file(file_path)
+
+    @staticmethod
+    def start_git():
+        print("Downloading Git...")
+        file_path = downloads.downloading("https://github.com/git-for-windows/git/releases/download/v2.41.0.windows.1/Git-2.41.0-64-bit.exe")
+        downloads.open_file(file_path)
+
+    @staticmethod
+    def start_docker():
+        print("Downloading Docker Desktop...")
+        file_path = downloads.downloading("https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe")
+        downloads.open_file(file_path, admin=True)
+
+    @staticmethod
+    def start_slack():
+        print("Downloading Slack...")
+        file_path = downloads.downloading("https://downloads.slack-edge.com/releases_windows/SlackSetup.exe")
+        downloads.open_file(file_path)
+
+    @staticmethod
+    def start_spotify():
+        print("Downloading Spotify...")
+        file_path = downloads.downloading("https://download.scdn.co/SpotifySetup.exe")
+        downloads.open_file(file_path)
+
+    @staticmethod
+    def start_python():
+        print("Downloading Python...")
+        file_path = downloads.downloading("https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe")
+        downloads.open_file(file_path)
+
+    @staticmethod
+    def start_nodejs():
+        print("Downloading Node.js...")
+        file_path = downloads.downloading("https://nodejs.org/dist/v18.17.1/node-v18.17.1-x64.msi")
+        downloads.open_file(file_path)
+
+    @staticmethod
+    def start_obs():
+        print("Downloading OBS Studio...")
+        file_path = downloads.downloading("https://cdn-fastly.obsproject.com/downloads/OBS-Studio-29.1.3-Full-Installer-x64.exe")
         downloads.open_file(file_path)
 
 class gui:
@@ -102,11 +161,21 @@ class gui:
         frame = ctk.CTkFrame(root)
         frame.pack(side="top", fill='x', pady=20)
 
-        brave_image = gui.image_resize((50, 50), "brave_icon.png")
-        vscode_image = gui.image_resize((50, 50), "vscode_icon.png")
-        zip_image = gui.image_resize((50, 50), "7zip_icon.png")
-        filterkeysetter_image = gui.image_resize((50, 50), "filterkeysetter_icon.png")
-        rainmeter_image = gui.image_resize((50, 50), "rainmeter_icon.png")
+        brave_image = gui.image_resize((50, 50), "icons/brave_icon.png")
+        vscode_image = gui.image_resize((50, 50), "icons/vscode_icon.png")
+        zip_image = gui.image_resize((50, 50), "icons/7zip_icon.png")
+        filterkeysetter_image = gui.image_resize((50, 50), "icons/filterkeysetter_icon.png")
+        rainmeter_image = gui.image_resize((50, 50), "icons/rainmeter_icon.png")
+        chrome_image = gui.image_resize((50, 50), "icons/chrome_icon.png")
+        notepadpp_image = gui.image_resize((50, 50), "icons/notepadpp_icon.png")
+        gimp_image = gui.image_resize((50, 50), "icons/gimp_icon.png")
+        git_image = gui.image_resize((50, 50), "icons/git_icon.png")
+        docker_image = gui.image_resize((50, 50), "icons/docker_icon.png")
+        slack_image = gui.image_resize((50, 50), "icons/slack_icon.png")
+        spotify_image = gui.image_resize((50, 50), "icons/spotify_icon.png")
+        python_image = gui.image_resize((50, 50), "icons/python_icon.png")
+        nodejs_image = gui.image_resize((50, 50), "icons/nodejs_icon.png")
+        obs_image = gui.image_resize((50, 50), "icons/obs_icon.png")
 
         brave_button = ctk.CTkButton(
             frame,
@@ -153,11 +222,111 @@ class gui:
             font=("Arial", 16, "bold")
         )
 
+        chrome_button = ctk.CTkButton(
+            frame,
+            text="Chrome",
+            command=downloads.start_chrome,
+            image=chrome_image,
+            compound="top",
+            font=("Arial", 16, "bold"),
+        )
+
+        notepadpp_button = ctk.CTkButton(
+            frame,
+            text="Notepad++",
+            command=downloads.start_notepadpp,
+            image=notepadpp_image,
+            compound="top",
+            font=("Arial", 16, "bold")
+        )
+
+        gimp_button = ctk.CTkButton(
+            frame,
+            text="GIMP",
+            command=downloads.start_gimp,
+            image=gimp_image,
+            compound="top",
+            font=("Arial", 16, "bold")
+        )
+
+        git_button = ctk.CTkButton(
+            frame,
+            text="Git",
+            command=downloads.start_git,
+            image=git_image,
+            compound="top",
+            font=("Arial", 16, "bold")
+        )
+
+        docker_button = ctk.CTkButton(
+            frame,
+            text="Docker",
+            command=downloads.start_docker,
+            image=docker_image,
+            compound="top",
+            font=("Arial", 16, "bold")
+        )
+
+        slack_button = ctk.CTkButton(
+            frame,
+            text="Slack",
+            command=downloads.start_slack,
+            image=slack_image,
+            compound="top",
+            font=("Arial", 16, "bold")
+        )
+
+        spotify_button = ctk.CTkButton(
+            frame,
+            text="Spotify",
+            command=downloads.start_spotify,
+            image=spotify_image,
+            compound="top",
+            font=("Arial", 16, "bold")
+        )
+
+        python_button = ctk.CTkButton(
+            frame,
+            text="Python",
+            command=downloads.start_python,
+            image=python_image,
+            compound="top",
+            font=("Arial", 16, "bold")
+        )
+
+        nodejs_button = ctk.CTkButton(
+            frame,
+            text="Node.js",
+            command=downloads.start_nodejs,
+            image=nodejs_image,
+            compound="top",
+            font=("Arial", 16, "bold")
+        )
+
+        obs_button = ctk.CTkButton(
+            frame,
+            text="OBS",
+            command=downloads.start_obs,
+            image=obs_image,
+            compound="top",
+            font=("Arial", 16, "bold")
+        )
+
         brave_button.pack(padx=20, pady=20, side="left")
         vscode_button.pack(padx=20, pady=20, side="left")
         zip_button.pack(padx=20, pady=20, side="left")
         filterkeysetter_button.pack(padx=20, pady=20, side="left")
         rainmeter_button.pack(padx=20, pady=20, side="left")
+        chrome_button.pack(padx=20, pady=20, side="left")
+        notepadpp_button.pack(padx=20, pady=20, side="left")
+        gimp_button.pack(padx=20, pady=20, side="left")
+        git_button.pack(padx=20, pady=20, side="left")
+        docker_button.pack(padx=20, pady=20, side="left")
+        slack_button.pack(padx=20, pady=20, side="left")
+        spotify_button.pack(padx=20, pady=20, side="left")
+        python_button.pack(padx=20, pady=20, side="left")
+        nodejs_button.pack(padx=20, pady=20, side="left")
+        obs_button.pack(padx=20, pady=20, side="left")
 
         root.mainloop()
 
